6e22f1790f916c43b7fd6c0732338a9740667965
Lucas Kennde <lucaskennde@gmail.com>
2024-05-29 21:58:27 -0300

    atualização final do projeto

--- scripts.js
+++ scripts.js
@@ -1,21 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+
     const links = document.querySelectorAll('.link');
     const megaman = document.querySelector('.megaman');
-    const piso = document.querySelector('.piso');
+    const piso = document.querySelector('.wrapperFooter');
     links.forEach(link => {
         link.addEventListener('click', function(event) {
-          // Obter a posição do offsetLeft do elemento
-        const newPosition = piso.offsetLeft;
-
-        // Obter a posição do background
-        const computedStyle = window.getComputedStyle(piso);
-        const backgroundPosition = computedStyle.backgroundPosition;
-
-        // Extrair o valor do backgroundPosition
-        const backgroundPositionX = parseFloat(backgroundPosition.split(' ')[0]);
-
-        console.log('Offset Left:', newPosition);
-        console.log('Background Position X:', backgroundPositionX);
+         
 
             if(link){
                 megaman.src = `./assets/megaWalk.gif`; 
@@ -28,4 +17,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 }, 600);
         });
     });
-});
